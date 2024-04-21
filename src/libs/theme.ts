75190f1caf6e3f0dfba3c33f539eee72e9522584
xkurozaru <kurozaru.210@gmail.com>
2024-04-22 02:19:37 +0900

    feat: add font

--- src/libs/theme.ts
+++ src/libs/theme.ts
@@ -1,10 +1,19 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import "@fontsource/noto-sans";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const fonts = {
+  body: "'Noto Sans JP', sans-serif",
+  heading: "'Noto Sans JP', sans-serif",
+};
+
+const theme = extendTheme({
+  config,
+  fonts,
+});
 
 export default theme;
