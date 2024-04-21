07e5d9bff59c0cb576bcb80181546ab17f27c7f2
xkurozaru <kurozaru.210@gmail.com>
2024-04-22 02:02:17 +0900

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
 import theme from "../libs/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={extendTheme(theme)}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={extendTheme(theme)}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
